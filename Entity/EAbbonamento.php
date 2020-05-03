52878b2c25d54ff29a971dda0f49455538df4527
anthony scarselli <anthonyscarselli@gmail.com>
2020-05-03 11:11:16 +0200

    classe entity abbonamento versione 1

---
+++ Entity/EAbbonamento.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class EAbbonamento
+{
+    function __construct($dataRinnovo , $importo)
+    {
+        $this->date = $dataRinnovo;
+        $this->import= $importo;
+    }
+
+    function setData($data) {
+        $this->date=$data;
+    }
+
+    function setImporto($importo_abb) {
+        $this->import=$importo_abb;
+    }
+
+    function getData() {
+        return $this->date;
+    }
+
+    function getImporto() {
+        return $this->import;
+    }
+
+    function toString() {
+        return "data rinnovo: ".$this->date." importo: ".$this->import."€";
+    }
+
+}
