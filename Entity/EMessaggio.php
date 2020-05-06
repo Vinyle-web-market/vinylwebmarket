74b9e4a48598f2b210e0a17faa1bd9f3fb8b5c20
Nannus7 <gianluca.nanni1.gn@gmail.com>
2020-05-06 12:59:25 +0200

    Classe EMessaggio versione1.

---
+++ Entity/EMessaggio.php
@@ -0,0 +1,100 @@
+<?php
+
+
+class EMessaggio
+{
+    private $id;
+    private $mittente;
+    private $destinatario;
+    private $oggetto;
+    private $testo;
+
+    /*Costruttore della classe Messaggio*/
+
+    function __construct($ID, $mitt, $dest, $ogg, $test)
+    {
+        $this->id = $ID;
+        $this->mittente = $mitt;
+        $this->destinatario = $dest;
+        $this->oggetto = $ogg;
+        $this->testo = $test;
+    }
+    /**
+     * @return mixed
+     */
+    function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @param mixed $id
+     */
+    function setId($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return mixed
+     */
+    function getMittente()
+    {
+        return $this->mittente;
+    }
+    /**
+     * @param mixed $mittente
+     */
+    function setMittente($mittente)
+    {
+        $this->mittente = $mittente;
+    }
+    /**
+     * @return mixed
+     */
+    function getDestinatario()
+    {
+        return $this->destinatario;
+    }
+    /**
+     * @param mixed $destinatario
+     */
+    function setDestinatario($destinatario)
+    {
+        $this->destinatario = $destinatario;
+    }
+    /**
+     * @return mixed
+     */
+    function getOggetto()
+    {
+        return $this->oggetto;
+    }
+    /**
+     * @param mixed $oggetto
+     */
+    function setOggetto($oggetto)
+    {
+        $this->oggetto = $oggetto;
+    }
+    /**
+     * @return mixed
+     */
+    function getTesto()
+    {
+        return $this->testo;
+    }
+    /**
+     * @param mixed $testo
+     */
+    function setTesto($testo)
+    {
+        $this->testo = $testo;
+    }
+    function toString()
+    {
+        return "Identificativo del messaggio: ".$this->id."\n" .
+            "Mittente del messaggio: ".$this->mittente."\n" .
+            "Destinatario del messaggio: ".$this->destinatario."\n" .
+            "Oggetto del messaggio: ".$this->oggetto."\n" .
+            "Testo del messaggio: ".$this->testo ."\n";
+    }
+}
