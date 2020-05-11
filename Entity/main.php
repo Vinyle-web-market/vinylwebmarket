c640671a62140d3e9f40d1d3c7a811718da06488
anthony scarselli <anthonyscarselli@gmail.com>
2020-05-11 10:50:54 +0200

    Aggiunta commenti

--- Entity/main.php
+++ Entity/main.php
@@ -1,7 +1,7 @@
 <?php
 include("EAbbonamento.php");
 include("ECarta.php");
-
+/*
 //prova di EAbbonamento se non serve escluderla mediante commento
 $data="08-12-2020";
 $importoAbb="0";
@@ -22,7 +22,8 @@ echo "numero mesi pagati: ".$nummesi."\n";
 $abb->AggiornaAbbonamento($nummesi);
 echo $abb->toString();
 
-
+// FINE CODICE PROVA
+*/
 
 /*
 //prova ECarta se non serve escluderla mediante commento
@@ -44,6 +45,7 @@ print "prova getNum ".$carta->getNum();
 print "prova getIntestat ".$carta->getIntestat();
 print "prova getCodcvv ".$carta->getCodcvv();
 print "prova getScad ".$carta->getScad();
+// FINE CODICE PROVA
 */
 
 ?>
