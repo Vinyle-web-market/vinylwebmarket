c818f1656a8e20b0bb4de55731d4b5aea083c6e8
Nannus7 <gianluca.nanni1.gn@gmail.com>
2020-05-22 13:35:49 +0200

    Prima parte di FMessaggio.

---
+++ Foundation/FMessaggio.php
@@ -0,0 +1,69 @@
+<?php
+
+
+class FMessaggio
+{
+    private static $table = "messaggio";
+
+    private static $class = "FMessaggio";
+
+    private static $values = (":id, :mittente, :destinatario, :testo, :oggetto");
+
+    public function __construct()
+    {
+    }
+
+    public static function bind($pdost, EMessaggio $messaggio)
+    {
+        $pdost->bindValue(':id', null, PDO::PARAM_INT);
+        $pdost->bindValue(':mittente', $messaggio->getMittente(), PDO::PARAM_STR);
+        $pdost->bindValue(':destinatario', $messaggio->getDestinatario(), PDO::PARAM_STR);
+        $pdost->bindValue(':testo', $messaggio->getTesto(), PDO::PARAM_STR);
+        $pdost->bindValue(':oggetto', $messaggio->getOggetto(), PDO::PARAM_STR);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getClass(): string
+    {
+        return self::$class;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTable(): string
+    {
+        return self::$table;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getValues(): string
+    {
+        return self::$values;
+    }
+
+    public function storeMessaggio($messaggio)
+    {
+        $db = FDataBase::getInstance();  /*se dovesse funzionare senza questa riga, dobbiamo eliminarla */
+        $id =$db->store(static::getClass(), $messaggio);
+        if ($id)
+            return $id;
+        else
+            return null;
+    }
+
+    public function existMessaggio($field, $id)
+    {
+        $exist = false;
+        $db = FDataBase::getInstance();  /*se dovesse funzionare senza questa riga, dobbiamo eliminarla */
+        $exist = $db->exist(static::getClass(), $field, $id);
+        if($exist)
+            return $exist = true;
+        else
+            return $exist = false;
+    }
+}
