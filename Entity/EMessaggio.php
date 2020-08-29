e5fd74b37234b24956484d1666c2a3579c05ff77
Nannus7 <gianluca.nanni1.gn@gmail.com>
2020-08-29 11:36:06 +0200

    Commenti su parte pacchetto Entity.

--- Entity/EMessaggio.php
+++ Entity/EMessaggio.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Enità Messaggio, dove sono presenti le caratteristiche di specifica e i suoi metodi peculiari.
+ * @author Gruppo Cruciani - Nanni - Scarselli
+ * @package Entity
+ */
 
 class EMessaggio
 {
@@ -9,11 +14,13 @@ class EMessaggio
      * autoincrement
      */
     private $id;
+
     /**
      * mittente del messaggio
      * References Utente_loggato
      */
     private $mittente;
+
     /**
      * destinatario del messaggio
      * References Utente_loggato
@@ -24,7 +31,12 @@ class EMessaggio
 
     private $testo;
 
-    /*Costruttore della classe Messaggio*/
+    /** Questo metodo è il costruttore della classe EMessaggio.
+     * EMessaggio constructor.
+     * @param $fname
+     * @param $data
+     * @param $type
+     */
 
     function __construct($mitt, $dest, $ogg, $test)
     {
@@ -33,9 +45,11 @@ class EMessaggio
         $this->oggetto = $ogg;
         $this->testo = $test;
     }
+
     /**Metodo getId che ci permette di prendere il valore di Id
      * @return mixed
      */
+
     function getId()
     {
         return $this->id;
@@ -44,6 +58,7 @@ class EMessaggio
     /**Metodo che ci permette di cambiare l'id di vinile
      * @param mixed $id
      */
+
     public function setId($id): void
     {
         $this->id = $id;
@@ -52,61 +67,80 @@ class EMessaggio
     /**Metodo getMittente che ci permettere di prendere il mittente
      * @return mixed
      */
+
     function getMittente()
     {
         return $this->mittente;
     }
+
     /**Metodo che ci permette di cambiare il mittente
      * @param mixed $mittente
      */
+
     function setMittente($mittente)
     {
         $this->mittente = $mittente;
     }
+
     /**Metodo che ci permette di prendere il valore di destinatario
      * @return mixed
      */
+
     function getDestinatario()
     {
         return $this->destinatario;
     }
+
     /**Metodo che ci permette di cambiare il destinatario
      * @param mixed $destinatario
      */
+
     function setDestinatario($destinatario)
     {
         $this->destinatario = $destinatario;
     }
+
     /** Metodo che ci permette di prendere il valore dell'oggetto
      * @return mixed
      */
+
     function getOggetto()
     {
         return $this->oggetto;
     }
+
     /**Metodo che ci permette di cambiare il valore dell'oggetto
      * @param mixed $oggetto
      */
+
     function setOggetto($oggetto)
     {
         $this->oggetto = $oggetto;
     }
+
     /**Metodo che ci permette di prendere il testo del messaggio
      * @return mixed
      */
+
     function getTesto()
     {
         return $this->testo;
     }
+
     /**Metodo che permette di cambiare il testo del messaggio
      * @param mixed $testo
      */
+
     function setTesto($testo)
     {
         $this->testo = $testo;
     }
-    /*Metodo che ci permette di stampare a video tutti gli
-     attributi della classe Messaggio*/
+
+    /**
+     * Metodo che restituisce una stringa con i dati relativi all'immagine.
+     * @return $st String
+     */
+
     function toString()
     {
         return
@@ -115,4 +149,5 @@ class EMessaggio
             "Oggetto del messaggio: ".$this->oggetto."\n" .
             "Testo del messaggio: ".$this->testo ."\n";
     }
+
 }
